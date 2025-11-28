abe6d50a7bb0b794ada97e7dc8ccab67cc0298bc
lazerfit <redkafe98@gmail.com>
2025-11-28 20:30:53 +0900

    init

---
+++ src/main/java/com/steamline/task_management_app_java/TaskManagementAppJavaApplication.java
@@ -0,0 +1,13 @@
+package com.steamline.task_management_app_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagementAppJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskManagementAppJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/steamline/task_management_app_java/TaskManagementAppJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.steamline.task_management_app_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagementAppJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
